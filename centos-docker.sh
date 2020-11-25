d53e713c8d5605d16e728bb3d061aa30898ff8e3
NicovVeldhuizen <58975704+NicovVeldhuizen@users.noreply.github.com>
2020-11-25 14:30:21 +0100

    Include instructions to script

--- centos-docker.sh
+++ centos-docker.sh
@@ -1,3 +1,14 @@
+# before downloading script, make sure the machine has it's IP setup and the hostname changed.
+# ifup <adapter ens192>
+# if command can't turn on the connection, change port in vsphere and try above command again.
+# sudo dhclient <adapter ens192>
+# sudo hostnamectl set-hostname <new hostname> 
+# download and run script using following commands:
+# wget <raw github link script.sh>
+# sudo chmod +x <script.sh>
+# ./<script.sh>
+
+# update and docker install
 #!/bin/bash
 # manually configured IP and set 8.8.8.8 as second DNS-server
 sudo dnf update
